1e8e7ef15a7a684a1b84ccaff9c0e56915e3bc76
Jeff Goeders <jeff.goeders@gmail.com>
2023-11-25 07:51:42 -0700

    reorg for randsoc

---
+++ rand_soc/main.py
@@ -0,0 +1,20 @@
+import argparse
+import pathlib
+from random import randint
+
+from rand_soc.creator import RandomDesign
+
+
+def main(output_dir_path, seed=None):
+    design = RandomDesign(seed=seed)
+    design.create()
+    design.write(output_dir_path / "design.tcl")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output_dir_path", type=pathlib.Path, help="Output directory path")
+    parser.add_argument("--seed", type=int, help="Random seed")
+    args = parser.parse_args()
+
+    main(args.output_dir_path, args.seed)
